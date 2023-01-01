6e88c0e0009c13c43baf9450b01b2f1a302156b3
MclPio <113801014+MclPio@users.noreply.github.com>
2023-01-01 16:29:00 -0500

    create working calculator in console

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function add(a,b) {
-    return a+b;
+    return +a + +b;
 }
 
 function subtract(a,b) {
-    return a-b;
+    return +a - +b;
 }
 
 function multiply(a,b) {
-    return a*b;
+    return +a * +b;
 }
 
 function divide(a,b) {
-    return a/b;
+    return +a / +b;
 }
 
 function operate(a,b,operator) {
@@ -23,14 +23,48 @@ function operate(a,b,operator) {
         return multiply(a,b);
     } else if (operator === "÷") {
         return divide(a,b);
-    }
+    } 
 }
 
+let n1;
+let n2;
+let o1;
+let o2;
+
 const numberButtons = document.querySelectorAll('.number');
-const display = document.querySelector('.display');
-let input = '';
-let userInput = numberButtons.forEach((item) => {
-    item.addEventListener('click', () => {
-    input+=item.textContent
-    display.textContent = input;
-})});
+const operatorButtons = document.querySelectorAll('.operators');
+
+//console.log(n1,n2,o1,o2,numberButtons,operatorButtons);
+
+numberButtons.forEach(number => number.addEventListener('click', () => {
+    if (!n1) {
+        n1 = number.textContent;
+    } else if (n1 && !o1) {
+        n1 += number.textContent;
+    } else if (n1 && o1 && !n2) {
+        n2 = number.textContent;
+    } else if (n1 && o1 && n2) {
+        n2 += number.textContent;
+    }
+    console.log(n1,n2);
+}));
+
+operatorButtons.forEach(operator => operator.addEventListener('click', () => {
+    if (operator.textContent === "=") {
+        if (n1 && n2 && o1) {
+            n1 = operate(n1,n2,o1);
+            n2 = null;
+            o1 = null;
+        }
+    } else if (operator.textContent === "+" || operator.textContent === "-" || operator.textContent === "×" || 
+    operator.textContent === "÷") {
+        if (n1 && !o1) {
+            o1 = operator.textContent;
+        } else if (n1 && o1 && n2) {
+            n1 = operate(n1,n2,o1);
+            o1 = operator.textContent;
+            n2 = null;
+        }
+    }
+    console.log(n1,n2,o1);
+}))
